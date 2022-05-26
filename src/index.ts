ac8528fd117970c96cfb15dfdfcddf9bfd6dfac2
Jay Hales <joshhales2004@gmail.com>
2022-05-26 20:56:56 +0100

    Finished.

--- src/index.ts
+++ src/index.ts
@@ -5,70 +5,66 @@ import ytpl from 'ytpl';
 import fs from 'fs';
 import Stream from 'stream';
 
-import NodeID3 from 'node-id3';
-
 const app = express();
 
-const COOKIE = {
-    headers: {
-        'x-youtube-identity-token': 'QUFFLUhqbFNpa05WbldGVzNsN3A3S3dPMzlVSlhlS19oQXw'
+app.get('/pl', async (req, res) => {
+    if (typeof(req.query.q) === 'string') {
+        const names = await downloadPlaylist(req.query.q)
+
+        res.end(
+            names.map(name => '<a href="' + name + '" download> ' + name.substring(8, name.length - 4) + '</a>').join('<br>')
+        );
     }
-}
+});
 
-app.get('/pl', (req, res) => {
+app.get('/songs/*', (req, res) => {
 
-    
+    console.log(decodeURI(req.url));
+
+    fs.readFile('.' + decodeURI(req.url), (err, data) => {
+        console.log(err);
+        res.contentType('audio/mpeg');
+        res.end(data.toString());
+    });    
+
 });
 
 async function downloadVideo(url: string) {
 
-    const filePath = './songs/' + getID(url) + '.mp3';
-
-    const ytStream = ytdl(url, {filter: 'audioonly', requestOptions: COOKIE});
-    const fsStream = ytStream.pipe(fs.createWriteStream(filePath));
-
-    const allPromises = await Promise.allSettled([Stream.once(fsStream, 'finish'), ytdl.getInfo(url)]);
-
-    //@ts-ignore
-    const ytInfo: ytdl.videoInfo = allPromises[1].value;
+    const ytInfo = await ytdl.getInfo(url);
 
     const trackName = ytInfo.videoDetails.media.song;
     const artistName = ytInfo.videoDetails.media.artist;
 
-    let fileTitle = '';
+    let fileTitle = artistName + ' ~ ' + trackName;
 
     if (trackName === undefined || artistName === undefined)
-        fileTitle = ytInfo.videoDetails.media.song || ytInfo.videoDetails.title.replace(/[/\\?%*:|"<>]/g, '-');   
-    else   
-        fileTitle = artistName + ' ~ ' + trackName
+        fileTitle = ytInfo.videoDetails.title.replace(/[/\\?%*:|"<>]/g, '-');  
+        
 
-    NodeID3.update({
-        artist: artistName,
-        title: trackName
-    }, filePath);
+    const finalFilePath = './songs/' + fileTitle + '.mp3';
 
-    fs.rename(filePath, './songs/' + fileTitle + '.mp3', () => {});
+    const ytStream = ytdl(url, {filter: 'audioonly'});
+    const fsStream = ytStream.pipe(fs.createWriteStream(finalFilePath));
 
-    return;
+    await Stream.once(fsStream, 'finish')
+
+    return finalFilePath;
 }
 
 async function downloadPlaylist(url: string) {
+
     const playlist = await ytpl(url);
 
-    const downloads: Promise<void>[] = [];
+    const downloads: Promise<string>[] = [];
+
 
     for (const item of playlist.items) {
         downloads.push(downloadVideo(item.shortUrl));
     }
 
-    await Promise.allSettled(downloads);
-    return;
+    const names = await Promise.all(downloads);
+    return names;
 }
 
-function getID(url: string): string {
-    return url.split('v=')[1];
-}
-
-downloadPlaylist('https://www.youtube.com/playlist?list=PLRBp0Fe2GpglkzuspoGv-mu7B2ce9_0Fn');
-//downloadVideo('https://www.youtube.com/watch?v=uNfsuth3J74')
-app.listen(3000)
+app.listen(3000);
